b5fdab518e6c21fb8bd54919b5d4c60d12838bc2
Jamie Edwards <jamieedwards@Jamies-MacBook-Pro.local>
2020-10-01 14:20:32 +0100

    Fix tests in basket service

--- clientApp/src/app/services/basket.service.spec.ts
+++ clientApp/src/app/services/basket.service.spec.ts
@@ -1,12 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { BasketService } from './basket.service';
+import { ProductService } from './product.service';
 
 describe('BasketService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  let service: BasketService;
+  let http: HttpClient;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    http = TestBed.get(HttpClient);
+    service = new BasketService(http, new ProductService(http));
+  });
 
   it('should be created', () => {
-    const service: BasketService = TestBed.get(BasketService);
     expect(service).toBeTruthy();
+  });
+
+  it('should have empty basket', (done) => {
+    service.basket$.subscribe(basket => {
+      expect(basket.items.length).toBe(0);
+      done();
+    });
   });
 });
